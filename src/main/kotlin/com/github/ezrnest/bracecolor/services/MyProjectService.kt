bee48284988d7e591a2d82a79b9e2b96b5a18f5b
GitHub Action <action@github.com>
2025-07-14 14:19:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/ezrnest/bracecolor/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ezrnest/bracecolor/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.ezrnest.bracecolor.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ezrnest.bracecolor.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
